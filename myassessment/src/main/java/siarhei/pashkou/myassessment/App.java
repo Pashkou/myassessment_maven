069941c38872a2320aacc75729a364d61a0014d9
pashkou <pashkou@pashkou-XPS-15-9560>
2018-12-26 11:10:17 +0100

    init

---
+++ myassessment/src/main/java/siarhei/pashkou/myassessment/App.java
@@ -0,0 +1,13 @@
+package siarhei.pashkou.myassessment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
